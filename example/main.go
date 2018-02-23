e4037909dae763a9759182999154d4cce507792c
ugjka <ugis.germanis@gmail.com>
2018-02-23 18:02:02 +0200

    fix example

--- example/main.go
+++ example/main.go
@@ -23,7 +23,7 @@ func main() {
 
 	// Loading Zone for Line Islands, Kiritimati
 	zone, err := gotz.GetZone(gotz.Point{
-		Lat: 1.74294, Lng: -157.21328,
+		Lat: 1.74294, Lon: -157.21328,
 	})
 	if err != nil {
 		panic(err)
